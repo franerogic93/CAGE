69352468490bc5f9b5a5331a0c4f67a88443c79b
frane rogic <Frane.Rogic@charax.de>
2024-01-18 10:06:05 +0100

    first commit

--- CAGE/Program.cs
+++ CAGE/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CAGE
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+        }
+}
