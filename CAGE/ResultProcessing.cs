db02b3cb5761e3e0799373310c68f3702d26e18b
frane rogic <Frane.Rogic@charax.de>
2024-01-18 10:14:56 +0100

    push

--- CAGE/ResultProcessing.cs
+++ CAGE/ResultProcessing.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace CAGE
 {
-    internal class ResultProcessing
+    public static class ResultProcessing
     {
+        //pairs up fish on Right and Left image
+        public static List<(string poinstLeft, string poinstRight)> pairUpFish(string txt)
+        {
+            return null;
+        }
+
+        //returns list of paired distances 
+        //left and right stero image
+        public static List<(double distanceLeft, double distanceRight)> CalculateFish(List<(string poinstLeft, string poinstRight)> fishPairs)
+        {
+            return null;
+        }
     }
 }
